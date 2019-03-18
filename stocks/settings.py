60be99b5b30b69655c596d384c8e2f0f19a151dd
Kirega <joseph.mutiga@gmail.com>
2019-03-18 14:14:37 +0300

    [ch #164593429]Update heroku configuration using

--- stocks/settings.py
+++ stocks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,7 +83,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'TEST': {
-            'NAME': os.getenv('DB_TEST') 
+            'NAME': os.getenv('DB_TEST')
         },
     }
 }
@@ -129,3 +130,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+django_heroku.settings(locals())
