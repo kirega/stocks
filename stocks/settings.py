e8f50652111dea2be2622d99eafbc238b4524fed
Kirega <joseph.mutiga@gmail.com>
2019-03-18 16:48:09 +0300

    [starts #164593972] Updates ALLOWED_HOST

--- stocks/settings.py
+++ stocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prox-stock.herokuapp.com','locahost', '127.0.0.1']
 
 
 # Application definition
