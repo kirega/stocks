fd2c88ec76872dd2d5c3b22798509435c88cfa07
Kirega <joseph.mutiga@gmail.com>
2019-03-18 15:49:36 +0300

    [finishes #164593429] drf urls wired

--- stocks/urls.py
+++ stocks/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framewrk.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
