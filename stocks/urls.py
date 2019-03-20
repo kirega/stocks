b6115db68f47ad440b4a24b2f0c00d6535f33557
Kirega <joseph.mutiga@gmail.com>
2019-03-20 12:29:39 +0300

    [ft #164593956] Product view wired via url

--- stocks/urls.py
+++ stocks/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('',include('products.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
