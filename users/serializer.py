52689b8eebf6381724dbfb0b2426136faf066753
Kirega <joseph.mutiga@gmail.com>
2019-03-22 10:41:07 +0300

    [finishes #164822860] Users model working

---
+++ users/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers 
+from django.contrib.auth.models import User
+# Create your views here.
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ('password',)
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name','email','password')
+        
