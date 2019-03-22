0d9db7952a8c20f4d997d313e31d5318c3ff7a73
Kirega <joseph.mutiga@gmail.com>
2019-03-22 03:38:46 +0300

    [finishes #164593972] stock moves record keeper

--- inventory/serializer.py
+++ inventory/serializer.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Inventory
-
+from products.serializer import ProductSerializer
+from products.models import Product
+from stores.serializer import StoreSerializer
+from stores.models import Store
 class InventorySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Inventory
         fields = ('id', 'store', 'product', 'quantity')
         depth = 1
- 
+        
+class InventoryCreateSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Inventory
+        fields = ('id', 'store', 'product', 'quantity')
--- inventory/views.py
+++ inventory/views.py
@@ -1,15 +1,20 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Inventory
-from .serializer import InventorySerializer
+from .serializer import InventorySerializer, InventoryCreateSerializer
 # Create your views here.
-class InventoryView(ListCreateAPIView):
+class InventoryView(ListAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
+class InventoryCreateView(CreateAPIView):
+    queryset = Inventory.objects.all()
+    serializer_class = InventoryCreateSerializer
+    permission_classes = (IsAuthenticated,)
+
 class InventoryDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Inventory.objects.all()
-    serializer_class = InventorySerializer
+    serializer_class = InventoryCreateSerializer
     permission_classes = (IsAuthenticated,)
     
